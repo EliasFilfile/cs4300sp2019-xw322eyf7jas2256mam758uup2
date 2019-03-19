b4344158e085388d4a5dedf72dd59b30de0a9488
Sherry Wang <xueyuanw@usc.edu>
2019-03-18 20:22:52 -0400

    Update Project Name

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,12 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "MOOC Recommendation System"
+net_id_1 = "Xueyuan Wang: xw322"
+net_id_2 = "Maxim Markovics: mam758"
+net_id_3 = "Elias Filfile: eyf7"
+net_id_4 = "Unmesh Padalkar: uup2"
+net_id_5 = "Jayadeep Shitole: jas2256"
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -14,7 +18,9 @@ def search():
 	else:
 		output_message = "Your search: " + query
 		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+	return render_template('search.html', name=project_name, 
+		netid=net_id_1, 
+		output_message=output_message, data=data)
 
 
 
