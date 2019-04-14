06109678267059879670046deb763ceed501eb50
Sherry Wang <xueyuanw@usc.edu>
2019-04-13 20:59:24 -0400

    Modify Name

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,7 +2,7 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "MOOC Recommendation System"
+project_name = "Reseach Paper Recommendation System"
 net_id_1 = "Xueyuan Wang: xw322"
 net_id_2 = "Maxim Markovics: mam758"
 net_id_3 = "Elias Filfile: eyf7"
@@ -14,9 +14,11 @@ def search():
 	query = request.args.get('search')
 	if not query:
 		data = []
-		output_message = ''
+		output_message = 'HERE output_message'
 	else:
 		output_message = "Your search: " + query
+		
+
 		data = range(5)
 	return render_template('search.html', 
 		name=project_name, 
