bf201ff813537b8028648507b5fc44f067a0ddaa
Sherry Wang <xueyuanw@usc.edu>
2019-04-15 22:40:17 -0400

    update files

--- search_query.py
+++ search_query.py
@@ -11,9 +11,11 @@ import nltk, string
 from nltk import word_tokenize
 from nltk import download
 from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+import urllib
                
-download('punkt')
-download('stopwords')
+#download('punkt')
+#download('stopwords')
                
 stop_words = stopwords.words('english')
 stemmer = nltk.stem.porter.PorterStemmer()
@@ -25,10 +27,8 @@ df = pd.read_pickle("ArxivData10k.pkl")
 dictionary = corpora.Dictionary(df['summary_proc'])
 # Load model and index
 lsi = LsiModel.load('LsiModel')  # load model
-print("===============================!!!!!!!!!!!!")
-print(lsi)
 lsi_index = MatrixSimilarity.load('MatrixSimilarity') # load index, (transformed corpus to LSI space and indexed it)
-print(lsi_index)
+
 
 def stem_tokens(tokens):
     return [stemmer.stem(item) for item in tokens if item not in stop_words]
@@ -41,33 +41,42 @@ def normalize(text):
 def search_func(arxiv_id, num_results = 10):
 	results = []
 	query_details = df[df['id'] == str(arxiv_id)].reset_index(drop=True)
+
 	try:
-		query = query_details.loc[0, 'summary']
+		title = query_details.loc[0, 'title']
+		link = query_details.loc[0, 'link']
+		summary = query_details.loc[0, 'summary']
 	except:
-		print("Paper not present in the database.")
-		return -1
+		url = "https://arxiv.org/abs/" + arxiv_id.replace("arXiv:","")
+		data = urllib.request.urlopen(url).read()
+		soup = BeautifulSoup(data, 'html.parser')
+		title = soup.find("meta",  property="og:title")['content']
+		link = soup.find("meta",  property="og:url")['content']
+		summary = soup.find("meta",  property="og:description")['content']
 
-	results.append([query_details.loc[0, 'title'], query_details.loc[0, 'link'], query_details.loc[0, 'summary']])
+	# Append the query 
+	# results.append([title, link, summary])
 
-	vec_bow = dictionary.doc2bow(normalize(query))
+	# Preprocess the query
+	vec_bow = dictionary.doc2bow(normalize(summary))
 	vec_lsi = lsi[vec_bow]  # convert the query to LSI space
 	sims = lsi_index[vec_lsi]  # perform a similarity query against the corpus
 	sims = sorted(enumerate(sims), key=lambda item: -item[1]) # sorted (document number, similarity score) 2-tuples
 
 	# Print Query details
-	# print("\nQuery:")
-	# print("Title:", query_details.loc[0, 'title'])
-	# print('Summary (query):', query)
+	print("\nQuery:")
+	print("Title:", title)
+	print('Summary (query):', summary)
 
 	# Print top 15 results
 	print("\n\nResults:\n")
 	for i in range(num_results):
 	    ind, score = sims[i]
-	    # print("Title: ", df.loc[ind, 'title'])
-	    # print("Link: ", df.loc[ind, 'link'])
-	    # print("Summary :", df.loc[ind,'summary'])
-	    # print(score)
-	    # print("\n")
+	    print("Title: ", df.loc[ind, 'title'])
+	    print("Link: ", df.loc[ind, 'link'])
+	    print("Summary :", df.loc[ind,'summary'])
+	    print(score)
+	    print("\n")
 	    result = [df.loc[ind, 'title'], df.loc[ind, 'link'], df.loc[ind,'summary']]
 	    results.append(result)
 	return results
@@ -77,4 +86,4 @@ if __name__ == '__main__':
 	num_results = int(sys.argv[2])
 	# results is a list of list, results[0] has query details and results[1 to num_results] has final results.
 	results = search_func(query, num_results)
-	print(results)
+	# print(results)
